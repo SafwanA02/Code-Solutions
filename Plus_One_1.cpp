d4aee330b839681969bbc5f516d8a521bdd5c1b9
SafwanA02 <107777797+SafwanA02@users.noreply.github.com>
2026-09-01 01:05:30 -0500

    Update Plus_One_2.cpp
    Used recursion to fix the solution

--- Plus_One_1.cpp
+++ Plus_One_1.cpp
@@ -1,23 +1,47 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int last_digit = 0;
-        last_digit = digits.size()-1;
+        int last_digit = digits.size()-1;
+        
         if (digits[last_digit] != 9){
             digits[last_digit] += 1;
             return digits;
         }
-        do{
-            digits[last_digit] = 0
-            if (digits[last_digit-1] == 9){
-                digits[last_digit-1] = 0;
-                last_digit -= 1;
+        else{
+            digits = check_for_nine(digits, last_digit);
+            
+            if (digits[0] == -1){
+                digits[0] = 1;
+                digits.push_back(0);
             }
 
+            return digits;
+        }
 
+        return digits;
+
+    }
+    vector<int> check_for_nine(vector<int>& digits, int i){
+        if ( i > 0){ //If it is not the first digit:
+            digits[i] = 0;
+            if (digits[i-1] == 9){
+                digits = check_for_nine(digits, i-1);
+            }
+            else{
+                digits[i-1] += 1;
+            }
+        }
+        else{
+            if (digits[i] == 9) {
+                digits[i] = -1;
+            }
+            else{
+                digits[i] += 1;
+            }
         }
 
         return digits;
 
     }
 };
+
