3d4acf40b6ca29e60fbd06691229c56ea7d17c2c
SafwanA02 <107777797+SafwanA02@users.noreply.github.com>
2026-09-02 23:20:16 -0500

    Create  67. Add Binary.cpp
    first attempt, failed miserably

---
+++ 67. Add Binary.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string answer = "";
+
+        int i = a.size() - 1;
+
+        for (int i = a.size()-1; i >=0; i--){
+            if (a[i])
+        }
+        while (i)
+
+        int a_num = a.size()-1;
+        int b_num = b.size()-1;
+        num = 0;
+
+        while (a_num >= 0 && b_num >=0){
+            num = a[a_num] + b[b_num];
+
+        }
+
+        if (a.size() > b.size()){
+            smaller_index = b.size() - 1;
+            larger_index = a.size() - 1;
+        }
+        else{
+            smaller_index = b.size() - 1;
+            larger_index = a.size() - 1;
+        }
+
+        while (smaller_index >=0){
+            if()
+        }
+
+        if (a.size() > b.size()){
+            last_index = b.size() - 1;
+        }
+        else{
+            last_index = a.size() - 1;
+        }
+        return answer;
+    }
+};
